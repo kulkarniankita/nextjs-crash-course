6a0378e84a960cc76486989c95b253fb1c708189
Ankita Kulkarni <kulkarni.ankita09@gmail.com>
2024-02-20 15:47:36 -0500

    generateStaticParams and [slug] page

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -1,3 +1,28 @@
-export default function Page({ params }: { params: { slug: string } }) {
-  return <div>My Post: {params.slug}</div>;
+import { createClient } from '@/supabase/client';
+
+export const revalidate = 0;
+
+export async function generateStaticParams() {
+  const supabase = createClient();
+  const { data: posts } = await supabase.from('easysell-products').select('id');
+
+  return posts?.map(({ id }) => ({
+    id,
+  }));
+}
+
+export default async function Page({ params }: { params: { slug: string } }) {
+  const supabase = createClient();
+  const { data } = await supabase
+    .from('easysell-products')
+    .select()
+    .match({ id: params.slug })
+    .single();
+
+  return (
+    <div>
+      My Post: {params.slug}
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
 }
