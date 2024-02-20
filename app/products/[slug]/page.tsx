914f862e736e6ce12d7fe5422f3ed3cbca39b209
Ankita Kulkarni <kulkarni.ankita09@gmail.com>
2024-02-20 15:48:37 -0500

    notfound page custom

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -1,4 +1,6 @@
 import { createClient } from '@/supabase/client';
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
 
 export const revalidate = 0;
 
@@ -18,6 +20,10 @@ export default async function Page({ params }: { params: { slug: string } }) {
     .select()
     .match({ id: params.slug })
     .single();
+
+  if (!data) {
+    notFound();
+  }
 
   return (
     <div>
