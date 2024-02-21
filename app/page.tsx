665fee6960e42c48a00912d29356454ff32f67c9
Ankita Kulkarni <kulkarni.ankita09@gmail.com>
2024-02-20 20:44:46 -0500

    notFound if no products

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,25 @@
 import Card from '@/components/card';
 import { createClient } from '@/supabase/client';
+import { notFound } from 'next/navigation';
 
 export const revalidate = 0;
 
 export default async function Home() {
   const supabase = createClient();
-  const { data } = await supabase.from('easysell-products').select();
+  const { data: products } = await supabase.from('easysell-products').select();
 
-  console.log({ data });
+  console.log({ products });
+
+  if (!products) {
+    return notFound();
+  }
 
   return (
     <main className="min-h-screen">
       <hr className="h-1 bg-purple-300 border-0 dark:bg-gray-700"></hr>
       <div className="px-12 py-12">
         <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-12">
-          {data?.map((item, idx) => (
+          {products.map((item, idx) => (
             <Card id={item.id} key={`${item.name}-${idx}`} {...item} />
           ))}
         </div>
